f316c864b00eeb645c5e4431de5586d9418a56b8
Radek <plocharadoslaw@gmail.com>
2023-06-05 20:59:44 +0200

    changed prod url

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = 'http://localhost:3001';
+const URL = 'https://social-media-backend-tfft.onrender.com';
 
 export const socket = io(URL,{
   autoConnect: false

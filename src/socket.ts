bb217499a88425b1ffd99fa34f6d052b1e646162
Radek <plocharadoslaw@gmail.com>
2023-06-10 14:46:01 +0200

    variable fixes

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const URL = 'https://social-media-backend-tfft.onrender.com';
+const URL = 'https://social-media-backend-tfft.onrender.com:3001';
 
 export const socket = io(URL);

98412a40285a5bc0d51630ba798a976dee4978a9
Radek <plocharadoslaw@gmail.com>
2023-06-11 15:43:39 +0200

    updated url

--- src/socket.ts
+++ src/socket.ts
@@ -2,4 +2,6 @@ import { io } from 'socket.io-client';
 
 const URL = 'https://social-media-backend-tfft.onrender.com:443';
 
-export const socket = io(URL);
+export const socket = io(URL,{
+    transports: ['websocket'],
+});
